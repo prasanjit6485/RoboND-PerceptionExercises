6396cb29e56afe4eabdc53decb4b47b1c9286b7f
Prasanjit Mondal <prasanjit6485@gmail.com>
2017-10-04 20:43:59 -0700

    feat: Implement RANSAC and outlier removal filtering using PCL Added RANSAC plane fitting to extract inliers and also, added outlier removal filtering using PCL to remove noise

--- Exercise-1/RANSAC.py
+++ Exercise-1/RANSAC.py
@@ -40,17 +40,45 @@ filename = 'pass_through_filtered.pcd'
 pcl.save(cloud_filtered, filename)
 
 # RANSAC plane segmentation
+# Create the segmentation object
+seg = cloud_filtered.make_segmenter()
 
+# Set the model you wish to fit 
+seg.set_model_type(pcl.SACMODEL_PLANE)
+seg.set_method_type(pcl.SAC_RANSAC)
+
+# Max distance for a point to be considered fitting the model
+# Experiment with different values for max_distance 
+# for segmenting the table
+max_distance = 0.01
+seg.set_distance_threshold(max_distance)
+
+# Call the segment function to obtain set of inlier indices and model coefficients
+inliers, coefficients = seg.segment()
 
 # Extract inliers
+extracted_inliers = cloud_filtered.extract(inliers, negative=True)
+filename = 'extracted_inliers.pcd'
+pcl.save(extracted_inliers, filename)
 
-# Save pcd for table
-# pcl.save(cloud, filename)
-
-
+# Optional
 # Extract outliers
+# Much like the previous filters, we start by creating a filter object: 
+outlier_filter = cloud_filtered.make_statistical_outlier_filter()
 
+# Set the number of neighboring points to analyze for any given point
+outlier_filter.set_mean_k(50)
 
-# Save pcd for tabletop objects
+# Set threshold scale factor
+x = 1.0
+
+# Any point with a mean distance larger than global (mean distance+x*std_dev) will be considered outlier
+outlier_filter.set_std_dev_mul_thresh(x)
+
+# Finally call the filter function for magic
+cloud_filtered = outlier_filter.filter()
+filename = 'outlier_removal.pcd'
+pcl.save(cloud_filtered, filename)
+
 
 
